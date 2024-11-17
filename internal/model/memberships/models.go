3ae9267ec9b22836072e7177e91c43ddfa86b2cb
Rifaissm01 <rifai.sinarsuksesmandiri.com>
2024-11-17 20:29:49 +0700

    users

--- internal/model/memberships/models.go
+++ internal/model/memberships/models.go
@@ -14,6 +14,7 @@ type (
 	UserModel struct {
 		ID        int64     `db:"id"`
 		Email     string    `db:"email"`
+		Username  string    `db:"username"`
 		Password  string    `db:"password"`
 		CreatedAt time.Time `db:"created_at"`
 		UpdatedAt time.Time `db:"updated_at"`
--- internal/repository/memberships/users.go
+++ internal/repository/memberships/users.go
@@ -1,1 +1,30 @@
 package memberships
+
+import (
+	"context"
+
+	"github.com/rifaiAhmed/fastcampus/internal/model/memberships"
+)
+
+func (r *repository) GetUser(ctx context.Context, email, username string) (*memberships.UserModel, error) {
+	query := `SELECT id, email, password, username, created_at, updated_at, created_by, updated_by 
+	FROM users WHERE email = $1 OR username $2`
+
+	row := r.db.QueryRowContext(ctx, query, email, username)
+	var response memberships.UserModel
+	err := row.Scan(&response.ID, &response.Email, &response.Username, &response.Password, &response.CreatedAt, &response.UpdatedAt, &response.UpdatedBy, &response.CreatedBy)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (r *repository) CreateUser(ctx context.Context, model memberships.UserModel) error {
+	query := `INSERT INTO users (email, password, username, created_at, updated_at, created_by, updated_by) VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	_, err := r.db.ExecContext(ctx, query, model.Email, model.Password, model.Username, model.CreatedAt, model.UpdatedAt, model.CreatedBy, model.UpdatedBy)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
